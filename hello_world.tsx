1865ce40eee6a2be68bd38bfe0f2af97f62f7e92
MiSawa <MiSawa@users.noreply.github.com>
2021-04-01 13:38:00 +0900

    Update hello_world.tsx

--- hello_world.tsx
+++ hello_world.tsx
@@ -4,18 +4,17 @@ import {
 } from "https://deno.land/x/sift@0.1.6/mod.ts";
 
 async function handleRequest(request: Request) {
-  if (request.method === 'GET') {
-    return jsx(
-        <html>
-          <head>
-            <title>Deno test</title>
-          </head>
-          <body>
-            Hello, deno!
-          </body>
-        </html>
-    );
-  }
+  console.log(request.method);
+  return jsx(
+      <html>
+        <head>
+          <title>Deno test</title>
+        </head>
+        <body>
+          Hello, deno!
+        </body>
+      </html>,
+  );
 }
 
 serve({
